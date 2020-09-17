afed08403fef2f0ca6ede62ebd3196058600feb1
Administrator <781047172@qq.com>
2020-09-17 16:32:46 +0800

    mes_agv2

---
+++ target/classes/cn/ac/casqiem/agv/orderchain/entity/OrderTemplate.java
@@ -0,0 +1,27 @@
+package cn.ac.casqiem.agv.orderchain.entity;
+
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
+public class OrderTemplate implements Serializable {
+    private Integer id;
+    private Integer orderChainTemplateId;
+    private Integer mapId;
+    private Integer action;
+    private Integer endPointId;
+    private Integer orient;
+    private Double offset;
+    private Integer layingoff;
+    private String info;
+    private Integer orderIndex;
+}
